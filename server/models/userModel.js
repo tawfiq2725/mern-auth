27fd0b6f97b429c183251b40f326aa133c184854
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2024-11-06 11:44:07 +0530

    User Model created

---
+++ server/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    user_name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
