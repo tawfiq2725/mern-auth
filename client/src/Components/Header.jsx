5b7b1b58200d54b8b4220c91964e35121080d714
Tawfiq Sirajudeen <tawfiqsiraj003003@gmail.com>
2024-11-06 18:13:48 +0530

    Complete signup UI

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <div className="bg-yellow-400">
-        <div className="flex justify-between items-center p-3 max-w-6xl mx-auto ">
+      <div className="bg-amber-200">
+        <div className="flex justify-between items-center p-5 max-w-6xl mx-auto ">
           <Link to="/">
-            <h1 className="font-bold">MERN-AUTH</h1>
+            <h1 className="font-bold text-2xl">MERN-AUTH</h1>
           </Link>
           <ul className=" flex gap-7 ">
             <Link to="/">
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <div className="p-4 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-10">Sign Up</h1>
+      <form className="flex justify-center flex-col gap-4">
+        <input
+          type="text"
+          placeholder="Name"
+          id="username"
+          className="bg-amber-100 p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          className="bg-amber-100 p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          className="bg-amber-100 p-3 rounded-lg"
+        />
+        <button className="bg-amber-800 text-yellow-50 p-3 rounded-lg hover:opacity-85 ">
+          Sign Up
+        </button>
+      </form>
+      <div className="mt-3">
+        Already have an account ?{" "}
+        <Link to="/signin">
+          <span className="text-blue-500">Sign In</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
